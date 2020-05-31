cc17f515532af0d350a9bf5c4e8c9b4489d65c40
hob71 <roberthobday@sky.com>
2020-05-31 21:40:53 +0000

    Addition of countdown and click count js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,29 @@
-var card = document.querySelector('.card');
-card.addEventListener( 'click', function() {
-  card.classList.toggle('rotation');
-});
+/*function start() {
+    timer();
+    clickcounter();
+}*/
+
+
+
+
+/*timer - RhymBil (youtube)*/
+function timer(){
+var time = 90;
+var Countdown = setInterval (function() {
+    --time;
+    document.getElementById("countdown").innerHTML = time;
+    if (time <= 0) {
+    time = 0+1;}
+},1000)}
+
+timer();
+
+/*clicker */
+
+var click = 0;
+function clickcounter() { 
+    click = click +1;
+    document.getElementById("clicker").innerHTML = click;
+}
 
 
